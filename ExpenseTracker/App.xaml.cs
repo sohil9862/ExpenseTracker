95256161ac09fbe7c7be9a123c3c5154cfe843a1
sohil9862 <sohilmaharjan00@gmail.com>
2024-12-18 15:58:36 +0545

    Add project files.

---
+++ ExpenseTracker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
